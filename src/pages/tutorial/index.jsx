f3fa3c7b5bfe30e617af7ec47711d3ef15df6cd1
yangbo <1832064870@qq.com>
2024-07-23 20:29:56 +0800

    兼容

--- src/pages/tutorial/index.jsx
+++ src/pages/tutorial/index.jsx
@@ -1,8 +1,8 @@
-import {useRef, useReducer, useEffect} from 'react'
+import {useRef, useReducer, useEffect, useState} from 'react'
 import {useOutlet, useLocation, useNavigate} from 'react-router-dom'
-import ActiveLink from 'components/ActiveLink'
-import {navigation} from 'pages/tutorial/path.js'
-import Tag from 'components/Tag'
+import SideNav from './sidenav'
+import Modal from 'components/Modal'
+import Button from 'components/Button'
 
 export default function Tutorial() {
   const navigate = useNavigate();
@@ -10,6 +10,7 @@ export default function Tutorial() {
   const outLet = useOutlet();
   const { pathname } = useLocation();
   const forceUpdate = useReducer(bool => !bool)[1]
+  const modal = useRef(null)
 
   if (!pageMap.current.has(pathname)) {
     pageMap.current.set(pathname, outLet)
@@ -23,6 +24,12 @@ export default function Tutorial() {
 
   return (
     <div className='pt-16 flex'>
+      <Button signal className='fixed bottom-6 right-6 z-40 md:hidden' onClick={() => modal.current.show()}>目录</Button>
+      <Modal ref={modal} onClose={() => modal.current.close()}>
+        <div className='bg-white h-full py-4 overflow-scroll'>
+          <SideNav />
+        </div>
+      </Modal>
       <nav className='hidden md:block w-60 sticky top-16 h-[calc(100vh_-_5rem)] overflow-y-auto bg-hei'>
         <div className='flex items-center px-4 py-8'>
           <div className='flex items-center justify-between p-1 border mr-3 rounded-md border-cyan-200'>
@@ -36,32 +43,9 @@ export default function Tutorial() {
             <div className='text-gray-500'>React@19.0.0-rc</div>
           </div>
         </div>
-        <ul className='border-r h-[calc(100vh_-_9rem_-_35px)] overflow-y-auto pb-8'>
-
-          {navigation.map((item, index) => {
-            if (item.type === 'tip') {
-              return <div className='mx-2 text-sm px-4 text-gray-400 first:pt-0 pb-4 pt-8' key={`z-${index}`}>
-                {item.name}
-              </div>
-            }
-            return (
-              <ActiveLink className='text-gray-700' activeName='text-blue-700' to={item.path} key={item.path} onClick={forceUpdate}>
-                <li key={item.path} className='mx-2 px-4 py-3 transition hover:bg-blue-100 text-sm flex items-center justify-between hover:text-blue-700 rounded'>
-                  <div className='flex items-center'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="currentColor" className="size-4">
-                      <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"/>
-                    </svg>
-                    <div className='ml-2 line-clamp-1'>{item.name}</div>
-                  </div>
-                  {item.label ? <Tag label={item.label} primary className='ml-2'/> : null}
-                </li>
-
-              </ActiveLink>
-            )
-          })}
-        </ul>
+          <ul className='border-r h-[calc(100vh_-_9rem_-_35px)] overflow-y-auto pb-8'>
+            <SideNav />
+          </ul>
       </nav>
       <div className='w-full md:w-[calc(100vw_-_15rem)] p-8 min-h-[calc(100vh_-_5rem)] box-border'>
         {/*<Outlet />*/}
